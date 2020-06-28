730550a9a84882794bf14050c9fd76e5b109d5e4
Edinaldobjr <edinaldobjr@hotmail.com>
2020-06-28 15:54:36 -0300

    testando

---
+++ Curso em Vídeo/Mundo 1/5 - sucessor e antecessor.py
@@ -0,0 +1,6 @@
+x = int(input("Digite um número inteiro: "))
+
+ante = x - 1
+suce = x + 1
+
+print('O número {} possui {} como antecessor e {} como sucessor.'.format(x, ante, suce))
---
+++ Curso em Vídeo/Mundo 1/6 - dobro tiplo e raiz.py
@@ -0,0 +1,8 @@
+x = int(input("Digite um número inteiro: "))
+
+dobro = x * 2
+triplo = x * 3
+raiz = x ** (1/2)
+
+print('O número {} possui {} como dobro, {} como triplo e {} como raiz.'.format(x, dobro, triplo, raiz))
+
