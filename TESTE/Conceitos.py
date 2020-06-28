39d733f2830c294ecdf63ff1d10697852cb15846
Edinaldobjr <edinaldobjr@hotmail.com>
2020-06-28 15:46:50 -0300

    testando

---
+++ TESTE/Conceitos.py
@@ -0,0 +1,20 @@
+print('O raio da esfera é: {:7.3f} cm'.format(R))
+
+"""
+{:f} = float, 
+{:d} = inteiro, 
+{:20} = imprime a string em 20 espaços,
+{:<20} = imprime alinhado a esquerda
+{:>20} = imprime alinhado a direita,
+{:^20} = imprime centralizado,
+{:=^20} = imprime centralizado com os faltantes completos de igual,
+{:.2f} = arredonda em 2 casas decimais apos a virgula
+{:07.2f} preencher com 0 até 7 casas total contando com a virgula e arredondar para duas casas apos a virgula 
+preenchendo com zero se precisar.
+
+print('O raio é: {} cm'.format(R), end='')
+end = '_' = coloca um caracter no final da linha nao pulando ela.
+
+print(f'Meu nome é {nome}')
+print(f'Meu nome é {nome.lower()}')
+"""
