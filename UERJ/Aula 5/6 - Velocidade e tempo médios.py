8d693d6f92c203083064601eebd16ddbd0588bcb
Edinaldobjr <edinaldobjr@hotmail.com>
2020-06-30 16:48:25 -0300

    UERJ

--- UERJ/Aula 5/6 - Velocidade e tempo médios.py
+++ UERJ/Aula 5/6 - Velocidade e tempo médios.py
@@ -1,6 +1,6 @@
 def mi_em_m(x):
     milhas = float(x)
-    metros = (1.61 * milhas) / 1000
+    metros = (1.61 * milhas) * 1000
     return metros
 
 
@@ -20,3 +20,14 @@ def seg_em_hora(x):
     segundos = int(x)
     hora = segundos * 3600
     return hora
+
+
+a = input('Qual o tempo em minutos? ')
+b = input('Qual a distancia em milhas? ')
+
+velocidade = mi_em_m(b) / min_em_seg(a)
+v_km = velocidade * 3.6
+print('A velocidade obtida é {:.2f} km/h.'.format(v_km))
+
+pace = seg_em_hora(min_em_seg(a))/(1000 * mi_em_m(b))
+print('O tempo por quilometro é de aproximadamente {:.2f} h.'.format(pace))
