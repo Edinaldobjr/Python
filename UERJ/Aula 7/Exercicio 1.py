ac51e6ed5d2eb36839ca78fe5177a6a64b37b326
Edinaldobjr <edinaldobjr@hotmail.com>
2020-07-21 16:08:59 -0300

    Alura e UERJ

--- UERJ/Aula 7/Exercicio 1.py
+++ UERJ/Aula 7/Exercicio 1.py
@@ -13,24 +13,23 @@ ok - identifique os eixos x e y
 
 x = np.linspace(0, 2, 21)
 
-y1 = x
-y2 = x**2
-y3 = x**3
-
-plt.plot(x, y1, 'b--')  # Traços Azuis
-plt.plot(x, y2, 'go')   # Bolas Verdes
-plt.plot(x, y3, 'y^')   # Triângulos Amarelos
+plt.plot(x, x, 'b--', label='Gráfico de x = y')  # Traços Azuis
+plt.plot(x, x**2, 'go', label='Gráfico de x = y²')   # Bolas Verdes
+plt.plot(x, x**3, 'y^', label='Gráfico de x = y³')   # Triângulos Amarelos
 
 plt.title('Gráficos')  # Título da Imagem
-
-plt.ylabel('Valores em Y')  # Label do Eixo y
 plt.xlabel('Valores em X')  # Label do Eixo x
+plt.ylabel('Valores em Y')  # Label do Eixo y
 
-plt.axis([0, 2, -0.5, 10])
+#plt.axis([0, 2, -0.5, 10])
+plt.ylim(-0.5, 10)
+plt.xlim(0, 2)
 
-plt.annotate('y = x**3', xy=(1.8, 6), xytext=(1, 9), arrowprops=dict(facecolor='yellow', shrink=0.05))
-plt.annotate('y = x**2', xy=(1.8, 3.2), xytext=(1, 7), arrowprops=dict(facecolor='green', shrink=0.05))
+'''
+plt.annotate('y = x³', xy=(1.8, 6), xytext=(1, 9), arrowprops=dict(facecolor='yellow', shrink=0.05))
+plt.annotate('y = x²', xy=(1.8, 3.2), xytext=(1, 7), arrowprops=dict(facecolor='green', shrink=0.05))
 plt.annotate('y = x', xy=(1.8, 1.7), xytext=(1, 5), arrowprops=dict(facecolor='blue', shrink=0.05))
+'''
 
-
+plt.legend(loc='upper left')
 plt.show()
